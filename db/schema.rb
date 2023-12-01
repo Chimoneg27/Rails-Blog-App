fde685a26c789937c26bf027e3dfe44b42297ace
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-12-01 18:48:14 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_29_231206) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_30_110826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_231206) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "commenteds", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "articles_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["articles_id"], name: "index_commenteds_on_articles_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -61,6 +70,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_231206) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "commenteds", "articles", column: "articles_id"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
