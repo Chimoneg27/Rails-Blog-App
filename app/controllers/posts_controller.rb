f53064e0a66776280b928d826903cdb79981cd5c
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-12-06 22:22:41 +0200

    Link controllers to database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find_by_id(params[:user_id])
+    @posts = Post.all
+  end
 
-  def show; end
+  def show
+    @post = Post.find_by_id(params[:id])
+    @comment = Comment.all
+    @user = User.find_by_id(params[:user_id])
+  end
 end
