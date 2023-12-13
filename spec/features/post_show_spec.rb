3e254bb432dd9ed1a967a43ee5afe7180626a2e8
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-12-13 21:27:00 +0000

    Create post_show_spec.rb

---
+++ spec/features/post_show_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+RSpec.describe 'User show', type: :feature do
+  before(:each) do
+    @user = FactoryBot.create(:user)
+    @post = FactoryBot.create(:post, author_id: @user.id)
+    @comments = FactoryBot.create_list(:comment, 3, post_id: @post.id)
+    visit user_posts_path(@user)
+  end
+  it 'shows the post\'s title' do
+    expect(page).to have_content(@post.title)
+  end
+  it 'shows who wrote the post' do
+    expect(page).to have_content(@user.name)
+  end
+  
+  it 'displays all comments authors' do
+    @comments.each do |comment|
+      username = User.find(comment.user_id).name
+      expect(page).to have_content(username)
+    end
+  end
+  it 'displays all comments' do
+    @comments.each do |comment|
+      expect(page).to have_content(comment.Text)
+    end
+  end
+end
