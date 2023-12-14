4ac0358803aa6272011be98f33cc28e8c2e39ee7
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-12-14 22:51:33 +0200

    Update routes and models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'user_id'
   has_many :comments, foreign_key: 'user_id'
@@ -6,7 +10,15 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  before_validation :set_default_posts_counter, on: :create
+
   def most_recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
+  end
+
+  private
+
+  def set_default_posts_counter
+    self.posts_counter ||= 0
   end
 end
