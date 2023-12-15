e99ca655d98238a7a61f8ad7920c462dc0e1f778
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-12-15 18:25:05 +0200

    modify ability and post models

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,18 +1,13 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
-
   def initialize(user)
-    def initialize(user)
-      user ||= User.new
-      if user.role == 'admin'
-        can :manage, :all
-      else
-        can :read, :all
-        can %i[destroy create], Comment, author_id: user.id
-        can %i[destroy create], Post, author_id: user.id
-      end
+    user ||= User.new
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :read, :all
+      can %i[destroy create], Comment, author_id: user.id
+      can %i[destroy create], Post, author_id: user.id
     end
   end
 end
